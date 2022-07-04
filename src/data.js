e726b2be2aade371222e9960a8b8255d0e802415
Trudeau Okech <trudeauokech@gmail.com>
2022-07-04 18:32:20 +0300

    added contact section

---
+++ src/data.js
@@ -0,0 +1,38 @@
+
+export const projects = [
+    {
+      title: "TODO LISTS",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./vadim-kaipov-f6jkAE1ZWuY-unsplash.jpg",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "Weather App",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "E-Commerce Site",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  export const skills = [
+   "PHP", "Javascript", "Laravel", "CSS", 
+  ];
